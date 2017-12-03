408c613b65c15f63a7440cbaf73ba39f05480740
Sergey Koposov <skoposov@cmu.edu>
2017-12-02 23:51:30 -0500

    fix the code

--- vel_fit.py
+++ vel_fit.py
@@ -6,7 +6,37 @@ import matplotlib.pyplot as plt
 import spec_fit
 import spec_inter
 import scipy.optimize
+import itertools
 
+def firstguess(specdata, options=None,
+         config=None,
+         resolParams=None):
+    minvel=-1000
+    maxvel=1000
+    velstep0 = 5
+    paramsgrid = {'logg':[1,2,3,4,5],
+                  'teff':[3000,5000,8000,10000],
+                  'feh':[-2,-1,0],
+                  'alpha':[0]}
+    vsinigrid = [None,10,100]
+    specParams = spec_inter.getSpecParams(specdata[0].name, config)
+    params = []
+    for x in itertools.product(*paramsgrid.values()):
+        curp = dict(zip(paramsgrid.keys(),x))
+        curp = [curp[_] for _ in specParams]
+        params.append(curp)
+    vels_grid = np.arange(minvel, maxvel, velstep0)
+
+    for vsini in vsinigrid:
+        if vsini is None:
+            rot_params  =None
+        else:
+            rot_params = (vsini,)
+            print (vsini)
+        res = spec_fit.find_best(specdata, vels_grid, params,
+                             rot_params, resolParams,
+                             config=config, options=options)
+    
 
 def doit(specdata, paramDict0, fixParam=None, options=None,
          config=None,
@@ -16,8 +46,8 @@ doit(specdata, {'logg':10, 'teff':30, 'alpha':0, 'feh':-1,'vsini':0}, fixParam =
                 config =config, resolParam = None)
 """
     maxnit = 5
-    minvel = -700
-    maxvel = 700
+    minvel = -1000
+    maxvel = 1000
     velstep0 = 5
     maxRotVel = 500
     minRotVel = 1e-2
@@ -105,8 +135,28 @@ doit(specdata, {'logg':10, 'teff':30, 'alpha':0, 'feh':-1,'vsini':0}, fixParam =
                                   options={'fatol': 1e-3, 'xatol': 1e-2})
     bestparam = paramMapper(res['x'])
     ret = {}
-    ret['param'] = dict(zip(specParams, bestparam))
+    ret['param'] = dict(zip(specParams, bestparam['params']))
     if fitVsini:
         ret['vsini'] = bestparam['vsini']
     ret['vel'] = bestparam['vel']
+    bestvel = bestparam['vel']
+    velstep = velstep0
+    while True:
+        vels_grid = np.concatenate((np.arange (bestvel, minvel, velstep)[::-1], np.arange(bestvel+velstep, maxvel, velstep)))
+        res1 = spec_fit.find_best(specdata, vels_grid, [[ret['param'][_] for _ in specParams]],
+                             bestparam['rot_params'], resolParams,
+                             config=config, options=options)
+        if res1['velerr'] < 10*velstep:
+            velstep/=4
+            minvel = max(minvel - 10 * res1['velerr'],minvel)
+            maxvel = min(maxvel + 10 * res1['velerr'],maxvel)
+        else:
+            break
+    chisq,yfit = spec_fit.get_chisq(specdata, bestvel
+                               ,[ret['param'][_] for _ in specParams], 
+                               bestparam['rot_params'],
+                               resolParams,
+                                    options=options, config=config,
+                                    getModel=True)
+    ret['yfit'] = yfit
     return ret
