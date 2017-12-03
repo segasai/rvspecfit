f4148936e761591d3bee3d82e416aebab38842ab
Sergey Koposov <koposov@ast.cam.ac.uk>
2017-12-03 17:51:23 -0500

    further doc change

--- utils.py
+++ utils.py
@@ -23,12 +23,39 @@ def get_revision():
         return ''
 
 def read_config(fname=None):
+    """
+    Read the configuration file and return the frozendict with it
+
+    Parameters:
+    -----------
+    fname: string, optional
+        The path to the configuration file. If not given config.yaml in the
+        current directory is used
+
+    Returns:
+    --------
+    config: frozendict
+        The dictionary with the configuration from a file
+    """
     if fname is None:
         fname = 'config.yaml'
     with open(fname) as fp:
         return freezeDict(yaml.safe_load(fp))
 
 def freezeDict(d):
+    """
+    Take the input object and if it is a dictionary, freeze it (i.e. return frozendict)
+    If not, do nothing
+
+    Parameters:
+    d: dict
+        Input dictionary
+
+    Returns:
+    --------
+    d: frozendict
+        Frozen input dictionary
+    """
     if isinstance(d, dict):
         d1 = {}
         for k, v in d.items():
