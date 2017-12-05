ca4c45f2d667c34ecad85e13a6165570f6952fae
Sergey Koposov <skoposov@cmu.edu>
2017-12-04 19:02:18 -0500

    style updates

--- desi/desi_fit.py
+++ desi/desi_fit.py
@@ -38,25 +38,25 @@ def procdesi(fname, ofname, fig_prefix):
     mws = tab['MWS_TARGET']
     targetid = tab['TARGETID']
     brick_name = tab['BRICKNAME']
-    xids  = np.nonzero(mws)[0]
-    setups = ('b','r','z')
-    fluxes = {} 
-    ivars = {} 
+    xids = np.nonzero(mws)[0]
+    setups = ('b', 'r', 'z')
+    fluxes = {}
+    ivars = {}
     waves = {}
     for s in setups:
-        fluxes[s] = pyfits.getdata(fname, '%s_FLUX'%s.upper())
-        ivars[s] = pyfits.getdata(fname, '%s_IVAR'%s.upper())
-        waves[s] = pyfits.getdata(fname, '%s_WAVELENGTH'%s.upper())
+        fluxes[s] = pyfits.getdata(fname, '%s_FLUX' % s.upper())
+        ivars[s] = pyfits.getdata(fname, '%s_IVAR' % s.upper())
+        waves[s] = pyfits.getdata(fname, '%s_WAVELENGTH' % s.upper())
 
-    outdict = {'brickname':[],
-               'target_id':[],
-               'vrad':[],
-               'vrad_err':[],
-               'logg':[],
-               'teff':[],
-               'vsini':[],
-               'feh':[],
-               'chisq':[]}
+    outdict = {'brickname': [],
+               'target_id': [],
+               'vrad': [],
+               'vrad_err': [],
+               'logg': [],
+               'teff': [],
+               'vsini': [],
+               'feh': [],
+               'chisq': []}
     large_error = 1e9
     for curid in xids:
         specdata = []
@@ -65,10 +65,10 @@ def procdesi(fname, ofname, fig_prefix):
         for s in setups:
             spec = fluxes[s][curid]
             curivars = ivars[s][curid]
-            curivars[curivars<=0] = 1./large_error**2
-            espec  = 1./curivars**.5
+            curivars[curivars <= 0] = 1. / large_error**2
+            espec = 1. / curivars**.5
             specdata.append(
-                spec_fit.SpecData('desi_%s'%s, 
+                spec_fit.SpecData('desi_%s' % s,
                                   waves[s], spec, espec))
         options = {'npoly': 15}
         res = fitter_ccf.fit(specdata, config)
@@ -87,43 +87,46 @@ def procdesi(fname, ofname, fig_prefix):
         outdict['feh'].append(res1['param']['feh'])
         outdict['chisq'].append(res1['chisq'])
         outdict['vsini'].append(res1['vsini'])
-        title='logg=%.1f teff=%.1f [Fe/H]=%.1f [alpha/Fe]=%.1f Vrad=%.1f+/-%.1f'%(res1['param']['logg'],
-                                                                                  res1['param']['teff'],
-                                                                                res1['param']['feh'],
-                                                                                  res1['param']['alpha'],
-                                                                                  res1['vel'],
-                                                                                  res1['vel_err'])
+        title = 'logg=%.1f teff=%.1f [Fe/H]=%.1f [alpha/Fe]=%.1f Vrad=%.1f+/-%.1f' % (res1['param']['logg'],
+                                                                                      res1['param']['teff'],
+                                                                                      res1['param']['feh'],
+                                                                                      res1['param']['alpha'],
+                                                                                      res1['vel'],
+                                                                                      res1['vel_err'])
         plt.clf()
-        plt.figure(1,figsize=(6, 6), dpi=300)
-        plt.subplot(3,1,1)
+        plt.figure(1, figsize=(6, 6), dpi=300)
+        plt.subplot(3, 1, 1)
         plt.plot(specdata[0].lam, specdata[0].spec, 'k-')
         plt.plot(specdata[0].lam, res1['yfit'][0], 'r-')
         plt.title(title)
-        plt.subplot(3,1,2)
+        plt.subplot(3, 1, 2)
         plt.plot(specdata[1].lam, specdata[1].spec, 'k-')
         plt.plot(specdata[1].lam, res1['yfit'][1], 'r-')
-        plt.subplot(3,1,3)
+        plt.subplot(3, 1, 3)
         plt.plot(specdata[2].lam, specdata[2].spec, 'k-')
         plt.plot(specdata[2].lam, res1['yfit'][2], 'r-')
         plt.xlabel(r'$\lambda$ [$\AA$]')
         plt.tight_layout()
-        plt.savefig(fig_prefix+'_%s_%d.png'%(curbrick, curtargetid))
-    outtab= astropy.table.Table(outdict)
-    outtab.write(ofname,overwrite=True)
+        plt.savefig(fig_prefix + '_%s_%d.png' % (curbrick, curtargetid))
+    outtab = astropy.table.Table(outdict)
+    outtab.write(ofname, overwrite=True)
 
 
-def procdesiWrapper(*args,**kwargs):
+def procdesiWrapper(*args, **kwargs):
     try:
         ret = procdesi(*args, **kwargs)
     except:
-        print ('failed with these arguments', args, kwargs)
+        print('failed with these arguments', args, kwargs)
         raise
+
+
 procdesiWrapper.__doc__ = procdesi.__doc__
+
 
 def domany(mask, oprefix, fig_prefix):
     """
     Process many spectral files
-    
+
     Parameters:
     -----------
     mask: string
@@ -134,20 +137,20 @@ def domany(mask, oprefix, fig_prefix):
         The prfix where the figures will be stored
     """
     skip_existing = True
-    fs= glob.glob(mask)
+    fs = glob.glob(mask)
     pool = mp.Pool(16)
-    res=[]
+    res = []
     for f in fs:
-        fname=f.split('/')[-1]
-        ofname = oprefix+'outtab_'+fname
+        fname = f.split('/')[-1]
+        ofname = oprefix + 'outtab_' + fname
         if skip_existing and os.path.exists(ofname):
-            print ('skipping, products already exist', f)
+            print('skipping, products already exist', f)
         res.append(pool.apply_async(procdesi, (f, ofname, fig_prefix)))
     for r in res:
         r.get()
-        
-    
-if __name__=='__main__':
+
+
+if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('mask',
                         help='mask files to be fitted, i.e. /tmp/desi*fits',
@@ -155,13 +158,12 @@ if __name__=='__main__':
     parser.add_argument('oprefix',
                         help='Output prefix for the tables',
                         type=str)
-    parser.add_argument('fig_prefix', 
+    parser.add_argument('fig_prefix',
                         help='Prefix for the fit figures',
                         type=str)
     args = parser.parse_args()
     mask = args.mask
     oprefix = args.oprefix
-    fig_prefix= args.fig_prefix
+    fig_prefix = args.fig_prefix
     domany(mask, oprefix, fig_prefix)
     #../../desi/dc17a2/spectra-64/1/134/spectra-64-134.fits
-
