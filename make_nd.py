b4bec28a21f9da9761e539285f595323b7a3d08a
Sergey Koposov <koposov@ast.cam.ac.uk>
2017-12-05 09:54:49 -0800

    code cleanup

--- make_nd.py
+++ make_nd.py
@@ -1,51 +1,73 @@
+import pickle
+import argparse
 import numpy as np
 import numpy.random
-import scipy.interpolate
-import os
-import subprocess
-from random import random, seed
-import pickle
+import scipy.spatial
 import dill
-import argparse
+import utils
 
+git_rev = utils.get_revision()
 
-def get_revision():
-    """ get the git revision of the code"""
-    try:
-        fname = os.path.dirname(os.path.realpath(__file__))
-        tmpout = subprocess.Popen(
-            'cd ' + fname + ' ; git log -n 1 --pretty=format:%H -- make_nd.py',
-            shell=True, bufsize=80, stdout=subprocess.PIPE).stdout
-        revision = tmpout.read()
-        return revision
-    except:
-        return ''
+def mapper(vec):
+    """
+    Map atmospheric parameters into parameters used in the grid for Interpolation
+    That includes logarithming the teff
 
+    Parameters:
+    -----------
+    vec: numpy array
+        The vector of atmospheric parameters Teff, logg, feh, alpha
 
-git_rev = get_revision()
-
-
-def mapper(v):
-    # map atmospheric parameters into parameters used in the grid
+    Returns:
+    ----------
+    ret: numpy array
+        The vector of transformed parameters used in interpolation
+    """
     import numpy as np
-    return np.array([np.log10(v[0]), v[1], v[2], v[3]])
+    return np.array([np.log10(vec[0]), vec[1], vec[2], vec[3]])
 
 
-def invmapper(v):
-    # inverse map
-    # map grid parameters into atmospheric parameters
+def invmapper(vec):
+    """
+    Map transformed parameters used in the grid for interpolation back into
+    the atmospheric parameters. That includes exponentiating the log10(teff)
+
+    Parameters:
+    -----------
+    vec: numpy array
+        The vector of transformed atmospheric parameters log(Teff), logg, feh, alpha
+
+    Returns:
+    ----------
+    ret: numpy array
+        The vector of original atmospheric parameters.
+    """
+
     import numpy as np
-    return np.array([10**v[0], v[1], v[2], v[3]])
-
+    return np.array([10**vec[0], vec[1], vec[2], vec[3]])
 
 def getedgevertices(vec):
-    # given a set of n-dimentional points
-    # return vertices of an n-dimensional cube that fully encompass the data
+    """
+    Given a set of n-dimentional points, return vertices of an n-dimensional
+    cube that fully encompass/surrounds the data, This is sort of the envelope
+    around the data
 
+    Parameters:
+    -----------
+    vec: numpy (Ndim, Npts)
+        The array of input points
+
+    Returns:
+    --------
+    vec: numpy (Ndim, Nretpts)
+        The returned array of surrounding points
+    """
+
+    pad = 0.2 # pad each dimension by this amount (relative to the dimension width)
     ndim = len(vec[:, 0])
     span = vec.ptp(axis=1)
-    lspans = vec.min(axis=1) - 0.2 * span
-    rspans = vec.max(axis=1) + 0.2 * span
+    lspans = vec.min(axis=1) - pad * span
+    rspans = vec.max(axis=1) + pad * span
     # edges of the data
     positions = []
     # the number of vertices in the cube is 2**ndim
@@ -60,12 +82,32 @@ def getedgevertices(vec):
     return positions
 
 
-def dosetup(HR, prefix=None):
-    "Prepare the N-d interpolation objects "
-    perturb = True
+def execute(spec_setup, prefix=None, perturb=True):
+    """
+    Prepare the triangulation objects for the set of spectral data for a given
+    spec_setup.
+
+    Parameters:
+    -----------
+    spec_setup: string
+        The spectroscopic configuration
+    prefix: string
+        The prefix where the data are located and where the triangulation will
+        be stored
+    perturb: boolean
+        Boolean flag whether to perturb a little bit the points before doing a
+        triangulation. This prevents issues with degenerate vertices and stability
+        of triangulation. Without perturbation find_simplex for example may revert
+        to brute force search.
+
+    Returns:
+    --------
+    None
+    """
+    perturbation_amplitude = 1e-6
 
     postf = ''
-    with open('%s/specs_%s%s.pkl' % (prefix, HR, postf), 'rb') as fp:
+    with open('%s/specs_%s%s.pkl' % (prefix, spec_setup, postf), 'rb') as fp:
         D = pickle.load(fp)
         vec, specs, lam, parnames = D['vec'], D['specs'], D['lam'], D['parnames']
         del D
@@ -74,14 +116,15 @@ def dosetup(HR, prefix=None):
     vec = mapper(vec)
     ndim = len(vec[:, 0])
 
+    # It turn's out that Delaunay is sometimes unstable when dealing with uniform
+    # grids, so perturb points
     if perturb:
-        seed(1)
-        # It turn's out that Delaunay is sometimes unstable whe
-        perturbation = 1e-6
-        vec = vec + np.random.uniform(-perturbation,
-                                      perturbation, size=vec.shape)
-
-    vec0 = vec.copy()
+        state = np.random.get_state()
+        # Make it deterministic
+        np.random.seed(1)
+        vec = vec + np.random.uniform(-perturbation_amplitude,
+                                      perturbation_amplitude, size=vec.shape)
+        np.random.set_state(state)
 
     # get the positions that are outside the existing grid
     edgepositions = getedgevertices(vec)
@@ -103,20 +146,20 @@ def dosetup(HR, prefix=None):
 
     triang = scipy.spatial.Delaunay(vec.T)
 
-    savefile = '%s/interp_%s%s.pkl' % (prefix, HR, postf)
-    dHash = {}
-    dHash['lam'] = lam
-    dHash['triang'] = triang
-    dHash['extraflags'] = extraflags
-    dHash['vec'] = vec
-    dHash['parnames'] = parnames
-    dHash['mapper'] = dill.dumps(mapper)
-    dHash['invmapper'] = dill.dumps(invmapper)
+    savefile = '%s/interp_%s%s.pkl' % (prefix, spec_setup, postf)
+    ret_dict = {}
+    ret_dict['lam'] = lam
+    ret_dict['triang'] = triang
+    ret_dict['extraflags'] = extraflags
+    ret_dict['vec'] = vec
+    ret_dict['parnames'] = parnames
+    ret_dict['mapper'] = dill.dumps(mapper)
+    ret_dict['invmapper'] = dill.dumps(invmapper)
 
     with open(savefile, 'wb') as fp:
-        pickle.dump(dHash, fp)
+        pickle.dump(ret_dict, fp)
     np.save('%s/interpdat_%s%s.npy' %
-            (prefix, HR, postf), np.asfortranarray(specs))
+            (prefix, spec_setup, postf), np.asfortranarray(specs))
 
 
 if __name__ == '__main__':
@@ -124,4 +167,4 @@ if __name__ == '__main__':
     parser.add_argument('--prefix', type=str)
     parser.add_argument('--setup', type=str)
     args = parser.parse_args()
-    dosetup(args.setup, args.prefix)
+    execute(args.setup, args.prefix)
