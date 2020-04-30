23c0a98858c9d13c088f2fdf67ee471d02b39529
Sergey Koposov <skoposov@cmu.edu>
2020-04-30 14:42:39 -0400

    fix the doc build

--- docs/conf.py
+++ docs/conf.py
@@ -17,6 +17,9 @@
 import os 
 import sys
 sys.path.insert(0, os.path.abspath('../py/'))
+fp=open('../py/rvspecfit/_version.py','w')
+print ('version="dev"',file=fp)
+fp.close()
 import rvspecfit
 
 # -- Project information -----------------------------------------------------
