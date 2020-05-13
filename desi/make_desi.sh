eaca74aa127b4d9245119f0a18f4898364f886b3
Sergey Koposov <skoposov@cmu.edu>
2020-05-13 00:07:23 -0400

    yank desi brz

--- desi/make_desi.sh
+++ desi/make_desi.sh
@@ -16,7 +16,6 @@ BRZSTEP=0.4
 BRESOL='x/1.55'
 RRESOL='x/1.55'
 ZRESOL='x/1.8'
-BRZRESOL='(x<7560)*(x/1.55)+(x>=7560)*(x/1.8)'
 BRZLAM0=$BLAM0
 BRZLAM1=$ZLAM1
 VSINIS=0,300
@@ -60,18 +59,4 @@ rvs_make_nd --prefix ${PREFIX}/ --setup desi_z --revision=$REVISION
 rvs_make_ccf --setup desi_z --lambda0 $ZLAM0 --lambda1 $ZLAM1  --every 30 \
     --vsinis $VSINIS --prefix ${PREFIX}/ --oprefix=${PREFIX} --step $ZSTEP \
     --revision=$REVISION
-
-
-# BRZ
-
-rvs_make_interpol --setup desi_brz --lambda0 $BRZLAM0 --lambda1 $BRZLAM1 \
-    --resol_func $BRZRESOL --step $BRZSTEP --templdb ${PREFIX}/files.db \
-    --oprefix ${PREFIX}/ --templprefix $TEMPLPREF --wavefile $WAVEFILE \
-    --revision=$REVISION --no-normalize
-
-rvs_make_nd --prefix ${PREFIX}/ --setup desi_brz --revision=$REVISION
-
-rvs_make_ccf --setup desi_brz --lambda0 $BRZLAM0 --lambda1 $BRZLAM1 \
-    --every 30 --vsinis $VSINIS --prefix ${PREFIX}/ --oprefix=${PREFIX} \
-    --step $BRZSTEP --revision=$REVISION
 
