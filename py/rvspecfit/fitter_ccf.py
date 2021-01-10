a5be0cbbc1906d157fd59159d5d1ea4f34b6a716
Sergey Koposov <skoposov@ed.ac.uk>
2021-01-09 21:06:38 -0500

    speed up and clarification of CCF code

--- py/rvspecfit/fitter_ccf.py
+++ py/rvspecfit/fitter_ccf.py
@@ -15,7 +15,8 @@ class CCFCache:
 
 def get_ccf_info(spec_setup, config):
     """
-    Returns the CCF info from the pickled file for a given spectroscopic spec_setup
+    Returns the CCF info from the pickled file for a given spectroscopic
+    setup
 
     Parameters
     -----------
@@ -65,8 +66,8 @@ def fit(specdata, config):
 
     Returns
     results: dict
-        The dictionary with results such as best template parameters, best velocity
-        best vsini.
+        The dictionary with results such as best template parameters,
+        best velocity, best vsini.
 
     """
     # configuration parameters
@@ -76,7 +77,6 @@ def fit(specdata, config):
     nvelgrid = int(2 * maxvel / (config.get('vel_step0') or 2))
     # number of points on the ccf in the specified velocity range
 
-    loglam = {}
     velstep = {}
     spec_fftconj = {}
     vels = {}
@@ -113,57 +113,55 @@ def fit(specdata, config):
         spec_fft = np.fft.fft(proc_spec)
         spec_fftconj[spec_setup] = spec_fft.conj()
         velstep[spec_setup] = (np.exp((logl1 - logl0) / npoints) - 1) * 3e5
-        l = len(spec_fft)
-        off[spec_setup] = l // 2
-        vels[spec_setup] = ((np.arange(l) + off[spec_setup]) % l -
+        lspec = len(spec_fft)
+        off[spec_setup] = lspec // 2
+        vels[spec_setup] = ((np.arange(lspec) + off[spec_setup]) % lspec -
                             off[spec_setup]) * velstep[spec_setup]
         vels[spec_setup] = -np.roll(vels[spec_setup], off[spec_setup])
         subind[spec_setup] = np.abs(vels[spec_setup]) < maxvel
 
-    maxv = -1e20
-    best_id = -90
-    best_v = -777
+    max_ccf = -np.inf
+    best_id = -1
 
     nfft = ccf_dats[spec_setup].shape[0]
     vel_grid = np.linspace(-maxvel, maxvel, nvelgrid)
     best_ccf = vel_grid * 0
-    for id in range(nfft):
+    for cur_id in range(nfft):
         curccf = {}
         for spec_setup in setups:
-            curf = ccf_dats[spec_setup][id, :]
+            curf = ccf_dats[spec_setup][cur_id, :]
             ccf = np.fft.ifft(spec_fftconj[spec_setup] * curf).real
             ccf = np.roll(ccf, off[spec_setup])
             curccf[spec_setup] = ccf[subind[spec_setup]]
-            curccf[spec_setup] = scipy.interpolate.UnivariateSpline(
+            curccf[spec_setup] = scipy.interpolate.CubicSpline(
                 vels[spec_setup][subind[spec_setup]][::-1],
-                curccf[spec_setup][::-1],
-                s=0)(vel_grid)
+                curccf[spec_setup][::-1])(vel_grid)
 
         allccf = ccf_combiner([curccf[_] for _ in setups])
-        if allccf.max() > maxv:
-            maxv = allccf.max()
-            best_id = id
-            best_v = vel_grid[np.argmax(allccf)]
-            best_model = {}
-            for spec_setup in setups:
-                best_model[spec_setup] = np.roll(
-                    ccf_mods[spec_setup][id],
-                    int(best_v / velstep[spec_setup]))
+        if allccf.max() > max_ccf:
+            max_ccf = allccf.max()
+            best_id = cur_id
+            best_vel = vel_grid[np.argmax(allccf)]
             best_ccf = allccf
-    try:
-        assert (best_id >= 0)
-    except:
-        raise Exception('Cross-correlation step failed')
+
+    if best_id < 0:
+        logging.error('Cross-correlation failed')
+        raise RuntimeError('Cross-correlation step failed')
+
+    best_model = {}
+    for spec_setup in setups:
+        best_model[spec_setup] = np.roll(ccf_mods[spec_setup][best_id],
+                                         int(best_vel / velstep[spec_setup]))
     best_par = ccf_infos[setups[0]]['params'][best_id]
     best_par = dict(zip(ccf_infos[setups[0]]['parnames'], best_par))
 
     best_vsini = ccf_infos[setups[0]]['vsinis'][best_id]
 
-    result = {}
-    result['best_par'] = best_par
-    result['best_vel'] = best_v
-    result['best_ccf'] = best_ccf
-    result['best_vsini'] = best_vsini
-    result['best_model'] = best_model
-    result['proc_spec'] = proc_specs
+    result = dict(best_par=best_par,
+                  best_vel=best_vel,
+                  best_ccf=best_ccf,
+                  best_vsini=best_vsini,
+                  best_model=best_model,
+                  proc_spec=proc_specs)
+
     return result
