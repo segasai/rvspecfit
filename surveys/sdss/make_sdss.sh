43c1de36ce6d76e3d39ed2593171492353ca0249
Sergey Koposov <skoposov@ed.ac.uk>
2021-02-02 08:29:25 -0500

    update the SDSS script to be more user friendly

--- surveys/sdss/make_sdss.sh
+++ surveys/sdss/make_sdss.sh
@@ -1,10 +1,18 @@
-#!/bin/bash -e 
+#!/bin/bash
+set -e
 NAME=sdss1
 WAVE0=3500
 WAVE1=9500
+OUTPUT_PREFIX=templ_data
+PHOENIXDIR=/home/skoposov/science/PHOENIX/v2.0/HiResFITS/PHOENIX-ACES-AGSS-COND-2011/
+PHOENIXWAVE=/home/skoposov/science/PHOENIX/v2.0/HiResFITS/WAVE_PHOENIX-ACES-AGSS-COND-2011.fits
 
-rvs_make_interpol --setup $NAME --lambda0 $WAVE0 --lambda1 $WAVE1 --resol 2000 --step 1 --templdb ../../templ_data/files.db --oprefix ../../templ_data/ --templprefix /home/skoposov/science/phoenix.astro.physik.uni-goettingen.de/v2.0/HiResFITS/PHOENIX-ACES-AGSS-COND-2011/ --wavefile /home/skoposov/science/phoenix.astro.physik.uni-goettingen.de/v2.0/HiResFITS/WAVE_PHOENIX-ACES-AGSS-COND-2011.fits
+# You may need this line if you haven't yet created the database of
+# templates yet
+# rvs_read_grid --prefix $PHOENIXDIR --templdb $OUTPUT_PREFIX/files.db
 
-rvs_make_nd --prefix ../../templ_data/ --setup $NAME
+rvs_make_interpol --setup $NAME --lambda0 $WAVE0 --lambda1 $WAVE1 --resol 2000 --step 1 --templdb $OUTPUT_PREFIX/files.db --oprefix $OUTPUT_PREFIX --templprefix $PHOENIXDIR --wavefile $PHOENIXWAVE
 
-rvs_make_ccf --setup $NAME --lambda0 $WAVE0 --lambda1 $WAVE1 --every 30 --vsinis 0,300 --prefix ../../templ_data/ --oprefix=../../templ_data --step 1
+rvs_make_nd --prefix $OUTPUT_PREFIX --setup $NAME
+
+rvs_make_ccf --setup $NAME --lambda0 $WAVE0 --lambda1 $WAVE1 --every 30 --vsinis 0,300 --prefix $OUTPUT_PREFIX --oprefix $OUTPUT_PREFIX --step 1
