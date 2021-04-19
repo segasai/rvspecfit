0705fdc9f8c3efd5adb9cb21176a8a249a23a3d3
Sergey Koposov <skoposov@ed.ac.uk>
2021-04-19 14:17:54 +0100

    remove untested parts from frozendict

--- py/rvspecfit/frozendict.py
+++ py/rvspecfit/frozendict.py
@@ -6,13 +6,7 @@
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 from collections import OrderedDict
-try:
-    # Python 3
-    from collections.abc import Mapping
-except ImportError:
-    # Python 2.7
-    from collections import Mapping
-
+from collections.abc import Mapping
 
 class frozendict(Mapping):
     """
@@ -60,6 +54,3 @@ class FrozenOrderedDict(frozendict):
 
     dict_cls = OrderedDict
 
-
-if OrderedDict is NotImplemented:
-    del FrozenOrderedDict
