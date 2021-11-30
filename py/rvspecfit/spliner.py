b979ab69b1a5fb0cc7c687925af4217ba1c98739
Sergey E. Koposov <skoposov@ed.ac.uk>
2021-11-30 13:54:58 +0000

    remove ffi.cast

--- py/rvspecfit/spliner.py
+++ py/rvspecfit/spliner.py
@@ -18,15 +18,14 @@ class Spline:
         D = np.zeros(N - 1, dtype=np.float64)
         h = np.zeros(N - 1, dtype=np.float64)
 
-        xs_c = ffi.cast('double *', ffi.from_buffer(xs))
-        ys_c = ffi.cast('double *', ffi.from_buffer(ys))
-        A_c = ffi.cast('double *', ffi.from_buffer(A))
-        B_c = ffi.cast('double *', ffi.from_buffer(B))
-        C_c = ffi.cast('double *', ffi.from_buffer(C))
-        D_c = ffi.cast('double *', ffi.from_buffer(D))
-        h_c = ffi.cast('double *', ffi.from_buffer(h))
         self.logstep = int(logstep)
-        _spliner.lib.construct(xs_c, ys_c, N, A_c, B_c, C_c, D_c, h_c)
+        _spliner.lib.construct(ffi.from_buffer('double *', xs),
+                               ffi.from_buffer('double *', ys), N,
+                               ffi.from_buffer('double *', A),
+                               ffi.from_buffer('double *', B),
+                               ffi.from_buffer('double *', C),
+                               ffi.from_buffer('double *', D),
+                               ffi.from_buffer('double *', h))
 
         self.N, self.A, self.B, self.C, self.D, self.h = N, A, B, C, D, h
         self.xs = xs
@@ -37,17 +36,17 @@ class Spline:
 
         nevalx = len(evalx)
         ret = np.zeros(nevalx, dtype=np.float64)
-        xs_c = ffi.cast('double *', ffi.from_buffer(self.xs))
-        A_c = ffi.cast('double *', ffi.from_buffer(self.A))
-        B_c = ffi.cast('double *', ffi.from_buffer(self.B))
-        C_c = ffi.cast('double *', ffi.from_buffer(self.C))
-        D_c = ffi.cast('double *', ffi.from_buffer(self.D))
-        h_c = ffi.cast('double *', ffi.from_buffer(self.h))
-        evalx_c = ffi.cast('double *', ffi.from_buffer(evalx))
-        ret_c = ffi.cast('double *', ffi.from_buffer(ret))
         nevalx = len(evalx)
-        stat = _spliner.lib.evaler(evalx_c, nevalx, self.N, xs_c, h_c, A_c,
-                                   B_c, C_c, D_c, self.logstep, ret_c)
+        stat = _spliner.lib.evaler(ffi.from_buffer('double *',
+                                                   evalx), nevalx, self.N,
+                                   ffi.from_buffer('double *', self.xs),
+                                   ffi.from_buffer('double *', self.h),
+                                   ffi.from_buffer('double *', self.A),
+                                   ffi.from_buffer('double *', self.B),
+                                   ffi.from_buffer('double *', self.C),
+                                   ffi.from_buffer('double *', self.D),
+                                   self.logstep,
+                                   ffi.from_buffer('double *', ret))
         assert (stat == 0)
 
         return ret
