74a38bc02faae7b106f2e047ed247f4ee22315d2
Sergey E. Koposov <skoposov@ed.ac.uk>
2023-01-27 19:08:29 +0000

    add comments to masking script

---
+++ surveys/mask_phoenix_grid.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+
+# This script masks the known problematic spectra in the PHOENIX grid
+# specifically the alpha=-0.4 cool stars and a couple of spectra 
+# in the middle of the grid
+# the arguments are input sql output sql file 
+
+
+set -e -u 
+if [ -f $1 ] ; then
+    {
+	echo cp $1 $2
+	echo '
+update files set bad=true where (alpha+0.4)<0.01 and teff<4500;
+update files set bad=true where 
+       abs(teff-3100)<1 and abs(logg-3)<0.01    and 
+       abs(met+.5)<0.01 and  abs(alpha-1.2)<0.01 ;
+update files set bad=true where 
+       abs(teff-3700)<1 and abs(logg-4)<0.01    and 
+       abs(met-.5)<0.01 and  abs(alpha-1.2)<0.01 ;
+ '| sqlite3 $2
+    }  ; else {
+    echo ' the first argument needs to be an sqlite db'
+    } ; fi
