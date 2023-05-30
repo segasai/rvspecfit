776956932bb515b5aed31072c46be696fbae7595
Sergey E. Koposov <skoposov@ed.ac.uk>
2023-05-30 23:32:06 +0100

    fix the resolution function

--- surveys/desi/make_desi.sh
+++ surveys/desi/make_desi.sh
@@ -15,7 +15,7 @@ BRESOL='x/1.55'
 RRESOL='x/1.55'
 ZRESOL='x/1.8'
 VSINIS=0,300
-REVISION=v230209
+REVISION=v230530
 EVERY=200
 SMOOTH=0.0
 PREFIX=../../..//templ_data/desi/${REVISION}/
@@ -31,14 +31,16 @@ LAM0=($BLAM0 $RLAM0 $ZLAM0)
 LAM1=($BLAM1 $RLAM1 $ZLAM1)
 STEP=($BSTEP $RSTEP $ZSTEP)
 CONF=(desi_b desi_r desi_z)
-       
+RESOL=($BRESOL $RRESOL $ZRESOL)
+
 for i in 0 1 2; do {
     CURLAM0=${LAM0[$i]}
     CURLAM1=${LAM1[$i]}
     CURCONF=${CONF[$i]}
     CURSTEP=${STEP[$i]}
+    CURRESOL=${RESOL[$i]}
     rvs_make_interpol --nthreads 2 --setup $CURCONF --lambda0 $CURLAM0 --lambda1 $CURLAM1 \
-    --resol_func $BRESOL --step $CURSTEP --templdb ${DBFILE} \
+    --resol_func $CURRESOL --step $CURSTEP --templdb ${DBFILE} \
     --oprefix ${PREFIX}/ --templprefix $TEMPLPREF --wavefile $WAVEFILE \
     --revision=$REVISION --no-normalize
 
