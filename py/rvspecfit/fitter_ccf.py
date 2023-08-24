segasai/rvspecfit

commit f466cab19c2797d036e192db8aa2a9e6aa0b3355

switch to rfft for performance

py/rvspecfit/fitter_ccf.py
@@ -114,8 +114,8 @@ def fit(specdata, config):
                                                         ccfconf=ccfconf)
         proc_specs[spec_setup] = proc_spec
         proc_ivars[spec_setup] = proc_ivar
-        spec_fft = np.fft.fft(proc_spec * proc_ivar)
-        ivar_fft = np.fft.fft(proc_ivar)
+        spec_fft = np.fft.rfft(proc_spec * proc_ivar)
+        ivar_fft = np.fft.rfft(proc_ivar)
 
         spec_fftconj[spec_setup] = spec_fft.conj()
         ivar_fftconj[spec_setup] = ivar_fft.conj()
@@ -158,8 +158,8 @@ def fit(specdata, config):
         for ii, spec_setup in enumerate(setups):
             curf = ccf_dats[spec_setup][cur_id, :]
             curf2 = ccf2_dats[spec_setup][cur_id, :]
-            curccf0 = np.fft.ifft(spec_fftconj[spec_setup] * curf).real
-            curccf1 = np.fft.ifft(ivar_fftconj[spec_setup] * curf2).real
+            curccf0 = np.fft.irfft(spec_fftconj[spec_setup] * curf)
+            curccf1 = np.fft.irfft(ivar_fftconj[spec_setup] * curf2)
             # chisquare i -2* S/E^2 xx T +  1/E^2 xx T^2
             # where xx is the convolution operator
             curccf.append(

py/rvspecfit/make_ccf.py
@@ -445,8 +445,8 @@ def ccf_executor(spec_setup,
                                                    vec,
                                                    ccfconf,
                                                    vsinis=vsinis)
-    ffts = np.array([np.fft.fft(x) for x in models])
-    fft2s = np.array([np.fft.fft(x**2) for x in models])
+    ffts = np.array([np.fft.rfft(x) for x in models])
+    fft2s = np.array([np.fft.rfft(x**2) for x in models])
     savefile = ('%s/' + CCF_PKL_NAME) % (oprefix, spec_setup)
     datsavefile = ('%s/' + CCF_DAT_NAME) % (oprefix, spec_setup)
     modsavefile = ('%s/' + CCF_MOD_NAME) % (oprefix, spec_setup)

commit fded13fdefe7ae35d061fed98f3f24c87ce36de3

Fix the bug introduced in commit: f466cab

py/rvspecfit/fitter_ccf.py
@@ -125,7 +125,9 @@ def fit(specdata, config):
         ivar_fftconj[spec_setup] = ivar_fft.conj()
 
         cur_step = (np.exp((logl1 - logl0) / npoints) - 1) * 3e5
-        lspec = len(spec_fft)
+        lspec = len(proc_spec)
+        # Importantly this has to be length of the spectrum rather than
+        # length of the fft since we are using rfft
         cur_off = lspec // 2
         # this is the wrapping point
         cur_vels = -((np.arange(lspec) + cur_off) % lspec - cur_off) * cur_step
@@ -175,6 +177,7 @@ def fit(specdata, config):
             # we interpolate all the ccf from every arm
             # to the same velocity grid
         all_chisqs.append(cur_chisq)
+
     all_chisqs = np.array(all_chisqs)
     best_id = np.argmin(all_chisqs.min(axis=1))
     best_ccf = all_chisqs[best_id]