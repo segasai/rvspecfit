535ac2ae5ec8d0c6b4e91b810ae0ce0d32ae4010
Sergey E. Koposov <skoposov@ed.ac.uk>
2024-07-21 20:09:36 +0100

    force training on cpu when testing

--- tests/make_templ_nn.sh
+++ tests/make_templ_nn.sh
@@ -21,4 +21,4 @@ RVS_MAKE_CCF="$COV `command -v rvs_make_ccf`"
 $RVS_READ_GRID --prefix $TEMPLPREF --templdb $PREFIX/files.db
 $RVS_MAKE_INTERPOL --air --setup $BNAME --lambda0 $BLAM0 --lambda1 $BLAM1 --resol $BRESOL --step $BSTEP --templdb ${PREFIX}/files.db --oprefix ${PREFIX}/ --templprefix $TEMPLPREF --fixed_fwhm --wavefile $WAVEFILE --nthreads 1
 
-$RVS_MAKE_ND --dir ${PREFIX}/ --pca_init --npc 10 --setup $BNAME
+$RVS_MAKE_ND --dir ${PREFIX}/ --pca_init --npc 10 --cpu --setup $BNAME
