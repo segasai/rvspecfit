d7220ea0fed3e3c73d156ae12660a0a4d52fcf9a
Sergey E. Koposov <skoposov@ed.ac.uk>
2024-07-30 18:13:25 +0100

    update generating script

--- tests/scripts/make_sdss_templ.sh
+++ tests/scripts/make_sdss_templ.sh
@@ -1,3 +1,4 @@
-rvs_make_interpol --setup sdss1 --lambda0 3500 --lambda1 9500 --resol 2000 --step 1 --templdb ./files_sdss.db --oprefix ./xtmp/ --templprefix /home/skoposov/science/PHOENIX/v2.0/HiResFITS/PHOENIX-ACES-AGSS-COND-2011/ --wavefile /home/skoposov/science/PHOENIX/v2.0/HiResFITS/WAVE_PHOENIX-ACES-AGSS-COND-2011.fits
-rvs_make_nd --prefix ./xtmp/ --setup sdss1
-rvs_make_ccf --setup sdss1 --lambda0 3500 --lambda1 9500 --every 30 --vsinis 0,300 --prefix ./xtmp/ --oprefix=./xtmp --step 1
+DIR=./templ_data_sdss
+rvs_make_interpol --setup sdss1 --lambda0 3500 --lambda1 9500 --resol 2000 --step 1 --templdb ./templates_sdss.db --oprefix $DIR --templprefix /home/skoposov/science/PHOENIX/v2.0/HiResFITS/PHOENIX-ACES-AGSS-COND-2011/ --wavefile /home/skoposov/science/PHOENIX/v2.0/HiResFITS/WAVE_PHOENIX-ACES-AGSS-COND-2011.fits
+rvs_make_nd --prefix $DIR --setup sdss1
+rvs_make_ccf --setup sdss1 --lambda0 3500 --lambda1 9500 --every 30 --vsinis 0,300 --prefix $DIR --oprefix=$DIR --step 1
