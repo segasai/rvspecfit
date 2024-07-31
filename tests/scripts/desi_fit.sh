35e5e8a7fdee46788474ac541bb34c405768b3c0
Sergey E. Koposov <skoposov@ed.ac.uk>
2024-07-31 18:42:06 +0100

    fix desi test

--- tests/scripts/desi_fit.sh
+++ tests/scripts/desi_fit.sh
@@ -1,4 +1,7 @@
 #!/bin/bash -e
 
-rvs_desi_fit  --output_dir ./tests_desi_output --no_subdirs  --minsn=2 --config tests/yamls/config_desi.yaml --objtypes='SCND_ANY,MWS_ANY,STD_*' --zbest_include --nthreads=2 ./tests/coadd-sv1-bright-10378.fits --process_status_file /tmp/tests_desi.status 
+rvs_desi_fit  --output_dir ./tests_desi_output --no_subdirs  --minsn=2 --config tests/yamls/config_desi.yaml --objtypes='SCND_ANY,MWS_ANY,STD_*' --zbest_include --nthreads=2 ./tests/data/coadd-sv1-bright-10378.fits --process_status_file /tmp/tests_desi.status 
+
+# 1 thread for coverage
+rvs_desi_fit  --output_dir ./tests_desi_output --no_subdirs --minsn=2 --config tests/yamls/config_desi.yaml --objtypes='SCND_ANY,MWS_ANY,STD_*' --zbest_include --nthreads=1 ./tests/data/coadd-sv1-bright-10378.fits --process_status_file /tmp/tests_desi.status 
 
