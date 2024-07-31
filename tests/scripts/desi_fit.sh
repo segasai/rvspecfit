c1fc2a100f22e8e0acd222368cf7162445ec1778
Sergey E. Koposov <skoposov@ed.ac.uk>
2024-07-31 22:10:21 +0100

    fix test

--- tests/scripts/desi_fit.sh
+++ tests/scripts/desi_fit.sh
@@ -9,9 +9,11 @@ $RVS_FIT  --output_dir ./tests_desi_output --no_subdirs  --minsn=2 --config test
 $RVS_FIT  --output_dir ./tests_desi_output --no_subdirs --minsn=2 --config tests/yamls/config_desi.yaml --objtypes='SCND_ANY,MWS_ANY,STD_*' --throw_exceptions --zbest_include --zbest_select --nthreads=1 --process_status_file /tmp/tests_desi.status  --log_level=DEBUG tests/data/coadd-sv1-bright-10378.fits 
 
 # specific targetids also plotting and zbest_select
-$RVS_FIT  --output_dir ./tests_desi_output --no_subdirs --minsn=2 --config tests/yamls/config_desi.yaml --objtypes='SCND_ANY,MWS_ANY,STD_*' --throw_exceptions --zbest_include --nthreads=1 --process_status_file /tmp/tests_desi.status  --log_level=DEBUG --targetid=39628422527323686,39628422531515839 --doplot tests/data/coadd-sv1-bright-10378.fits
+echo 39628422527323686 > /tmp/targetids
+echo 39628422531515839 >> /tmp/targetids
+$RVS_FIT  --output_dir ./tests_desi_output --no_subdirs --minsn=2 --config tests/yamls/config_desi.yaml --objtypes='SCND_ANY,MWS_ANY,STD_*' --throw_exceptions --zbest_include --nthreads=1 --process_status_file /tmp/tests_desi.status  --log_level=DEBUG --targetid_file_from=/tmp/targetids --doplot tests/data/coadd-sv1-bright-10378.fits
 
 
 echo tests/data/coadd-sv1-bright-10378.fits > /tmp/queue.list
 # queue
-$RVS_FIT  --output_dir ./tests_desi_output --no_subdirs --minsn=2 --config tests/yamls/config_desi.yaml --objtypes='SCND_ANY,MWS_ANY,STD_*' --throw_exceptions --zbest_include --nthreads=1 --process_status_file /tmp/tests_desi.status  --log_level=DEBUG --queue_file=/tmp/queue.list
+$RVS_FIT  --output_dir ./tests_desi_output --no_subdirs --minsn=2 --config tests/yamls/config_desi.yaml --objtypes='SCND_ANY,MWS_ANY,STD_*' --throw_exceptions --zbest_include --nthreads=1 --process_status_file /tmp/tests_desi.status  --log_level=DEBUG --queue_file --input_file_from=/tmp/queue.list
