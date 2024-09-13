b5d14c94283d725653193919c33741fccfbf48ce
Sergey E. Koposov <skoposov@ed.ac.uk>
2024-09-13 13:26:29 +0100

    test resolution matrix

--- tests/scripts/desi_fit.sh
+++ tests/scripts/desi_fit.sh
@@ -8,6 +8,9 @@ $RVS_FIT  --output_dir ./tests_desi_output --no_subdirs  --minsn=2 --config test
 # 1 thread for coverage
 $RVS_FIT  --output_dir ./tests_desi_output --no_subdirs --minsn=2 --config tests/yamls/config_desi.yaml --objtypes='SCND_ANY,MWS_ANY,STD_*' --throw_exceptions --zbest_include --zbest_select --nthreads=1 --process_status_file /tmp/tests_desi.status  --log_level=DEBUG tests/data/coadd-sv1-bright-10378.fits 
 
+# 1 resolution
+$RVS_FIT  --output_dir ./tests_desi_output --resolution_matrix --no_subdirs --minsn=2 --config tests/yamls/config_desi.yaml --objtypes='SCND_ANY,MWS_ANY,STD_*' --throw_exceptions --zbest_include --zbest_select --nthreads=1 --process_status_file /tmp/tests_desi.status  --log_level=DEBUG tests/data/coadd-sv1-bright-10378.fits 
+
 # specific targetids also plotting and zbest_select
 echo 39628422527323686 > /tmp/targetids
 echo 39628422531515839 >> /tmp/targetids
